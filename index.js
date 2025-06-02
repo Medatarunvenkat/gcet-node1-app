4981076f32711c65b7316d321881601bb1a90184
Medatarunvenkat <tarunvm0909@gmail.com>
2025-06-02 14:05:09 +0530

    done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.listen(8080,()=>{
     console.log("Server Started on port 8080");
 });
@@ -13,5 +15,19 @@ app.get("/greet",(req,res)=>{
 });
 
 app.get("/name",(req,res)=>{
-  return res.send("Hello Tarun");
+  return res.send("<h1>Hello Tarun</h1>");
 });
+
+app.get("/weather",(req,res)=>{
+  return res.send("Weather is sunny 40 degrees");
+})
+
+const products = [
+  { id: 1,name:"Product 1",price:100},
+  { id: 2,name:"Product 2",price:200},
+  { id: 3,name:"Product 3",price:300}
+];
+app.get("/products",(req,res)=>{
+  
+  res.send(products);
+})
