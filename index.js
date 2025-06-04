2314fbba34831efe07f0eefefd063b3f1ed2c742
Medatarunvenkat <tarunvm0909@gmail.com>
2025-06-04 14:07:22 +0530

    now

--- index.js
+++ index.js
@@ -119,7 +119,13 @@ const MONGODB_URI=process.env.MONGODB_URI
 app.use("/users", userRouter);
 app.use("/products", productRouter);
 app.use("/order",orderRouter);
-app.listen(8080, () => {
-  mongoose.connect(`${MONGODB_URI}`);
-  console.log("Server Started");
-});
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server Started on port 8080");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
